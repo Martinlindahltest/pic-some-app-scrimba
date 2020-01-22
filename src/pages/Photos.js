06acd2359e8a7b04aa24ebad183ada20c5b71fb2
Martinlindahltest <basmartin@gmail.com>
2020-01-22 15:43:57 +0100

    innan 5 har börjat

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 export default function Photos() {
+
+    // Get the allPhotos array from context
+    // map over it, creating <Image /> elements of the component we just made
+    // <Image key={???} img={<full image object here>} className={getClass(<index of image>)} />
+
     return (
+
         <main className="photos">
             <h1>Images go here</h1>
         </main>
