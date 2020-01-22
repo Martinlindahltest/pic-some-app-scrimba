afa11581f0253d4ee799c8e2f39c94c02f093e37
Martinlindahltest <basmartin@gmail.com>
2020-01-22 15:33:09 +0100

    Part 5 fungerar

--- src/Context.js
+++ src/Context.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Context = React.createContext()
 
 function ContextProvider({ children }) {
     const [allPhotos, setAllPhotos] = useState([]);
+
+    async function fetchData() {
+        const res = await fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json");
+        res
+            .json()
+            .then(res => setAllPhotos(res))
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
 
     return (
         <Context.Provider value={{ allPhotos: allPhotos }}>
