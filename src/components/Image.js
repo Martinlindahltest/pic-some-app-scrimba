804d0d1c49a28638c99c80283da7e87c644a045a
Martinlindahltest <basmartin@gmail.com>
2020-01-23 11:34:32 +0100

    fungerar lekt 7 tror jag

--- src/components/Image.js
+++ src/components/Image.js
@@ -6,9 +6,14 @@ function Image({ className, img }) {
 
     console.log(img.id, hovered)
 
+    const heartIcon = hovered ? <i className="ri-heart-line favorite"></i> : null
+    const cartIcon = hovered ? <i className="ri-add-circle-line cart"></i> : null
+
     return (
         <div className={`${className} image-container`}>
             <img onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)} alt='' src={img.url} className="image-grid" />
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
