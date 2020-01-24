28126f355169721397dfe733851e212b151b2001
Martinlindahltest <basmartin@gmail.com>
2020-01-24 09:59:55 +0100

    lektion 9 fungerar

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,9 +4,15 @@ function Image({ className, img, toggleFavorite }) {
 
     const [hovered, setHovered] = useState(false)
 
-    //console.log(img.id, hovered)
+    //console.log('img.id', img.id, 'img.isFavorite', img.isFavorite)
 
-    const heartIcon = hovered ? <i onClick={() => toggleFavorite(img.id)} className="ri-heart-line favorite"></i> : null
+    let heartClassName = "ri-heart-line favorite"
+
+    if (img.isFavorite) {
+        heartClassName = "ri-heart-fill favorite"
+    }
+
+    const heartIcon = hovered ? <i onClick={() => toggleFavorite(img.id)} className={heartClassName}></i> : null
     const cartIcon = hovered ? <i className="ri-add-circle-line cart"></i> : null
 
     return (
